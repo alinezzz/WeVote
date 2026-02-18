ec7aa98dc29179bd4a93ce9028836c12a9c2303c
alinezzz <161722152+alinezzz@users.noreply.github.com>
2026-02-18 14:56:10 +0300

    Главный экран (без карточек)

--- app/src/main/java/com/example/wevote/AuthActivity.kt
+++ app/src/main/java/com/example/wevote/AuthActivity.kt
@@ -2,6 +2,7 @@ package com.example.wevote
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,6 +14,12 @@ class AuthActivity : AppCompatActivity() {
         val registerText = findViewById<TextView>(R.id.text_register)
         registerText.setOnClickListener {
             val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
+        val continueButton = findViewById<Button>(R.id.button_continue)
+        continueButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/wevote/RegistrationActivity.kt
+++ app/src/main/java/com/example/wevote/RegistrationActivity.kt
@@ -3,6 +3,7 @@ package com.example.wevote
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 
 class RegistrationActivity : AppCompatActivity() {
@@ -13,6 +14,12 @@ class RegistrationActivity : AppCompatActivity() {
         val backButton = findViewById<ImageButton>(R.id.bt_back)
         backButton.setOnClickListener {
             val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+        }
+
+        val registerButton = findViewById<Button>(R.id.button_register)
+        registerButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
     }
